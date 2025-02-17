5d28dbba2e9b6990d87b8405fca9d0b8d15aaf39
Dhrumil <dhrumilpatel8590@gmail.com>
2025-02-17 15:37:17 +0530

    Add locomotive scroll animation and new video file for enhanced page transitions

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
 function loadAnimation() {
   var tl = gsap.timeline();
   tl.from(".line h1", {
@@ -21,13 +59,14 @@ function loadAnimation() {
     },
   });
   tl.to(".line h2", {
-    animationName: "anime",
+    animationName: "loaderAnime",
     opacity: 1,
   });
   tl.to("#loader", {
     opacity: 0,
     duration: 0.2,
     delay: 0,
+    // delay: 3,
   });
   tl.from("#page1", {
     delay: 0.2,
@@ -45,6 +84,9 @@ function loadAnimation() {
     y: 140,
     stagger: 0.2,
   });
+  tl.from("#hero1,#page2", {
+    opacity: 0,
+  }, "-=1.2");
 }
 function cursorAnimation() {
   document.addEventListener("mousemove", function (dets) {
@@ -57,3 +99,4 @@ function cursorAnimation() {
 }
 loadAnimation();
 cursorAnimation();
+locomotiveAnimation();
